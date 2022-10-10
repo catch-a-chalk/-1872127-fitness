6b93d03d861c8528355a481b90562e2e6d72c026
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-10-10 21:01:19 +0300

    Show example swiper

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {initVideo} from './modules/init-video';
 import {initPlanTabs} from './modules/init-plantabs';
+import { initSwiper } from './modules/init-swiper';
 
 // ---------------------------------
 
@@ -17,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   initVideo();
   initPlanTabs();
+  initSwiper();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
